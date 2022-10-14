119f569cad9b0d48a182582f5358f3d490865580
alejandrogzh <omar_guzman@pegaduro.com>
2022-10-14 09:55:48 -0500

    cambios en los controllers

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,44 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    try {
+      createPokemonDto.name = createPokemonDto.name.toLowerCase();
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`Pokemon exists in db ${createPokemonDto.name}`) 
+      }
+      console.log(error)
+      throw new InternalServerErrorException(`Can't create Pokemon - Check Server Errors`)
+    }
+  }
+
+  findAll() {
+    return this.pokemonModel.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
